d5f9765fec016ce2e3fd4d1f35a9173f5f7bf612
shimyon <shimyon@hotmail.com>
2023-07-21 13:34:23 +0530

    changes

--- routes/templateRoutes.js
+++ routes/templateRoutes.js
@@ -3,11 +3,11 @@ const router = express.Router()
 
 const { add, getById, update, getAll,remove,setDefault } = require('../controllers/templateController')
 const { protect } = require('../middleware/authMiddleware')
-router.post('/add',  add)
-router.post('/update', update)
-router.post('/getAll', getAll)
-router.post('/remove/:id', remove)
-router.get('/getById/:id', getById)
-router.post('/setDefault', setDefault)
+router.post('/add',protect,  add)
+router.post('/update',protect, update)
+router.post('/getAll',protect, getAll)
+router.post('/remove/:id',protect, remove)
+router.get('/getById/:id',protect, getById)
+router.post('/setDefault',protect, setDefault)
 
 module.exports = router
